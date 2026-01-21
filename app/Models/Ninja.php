af834793f1cf81309dae944ddf9bfbb3d0fe1b18
swathysukumaran <swathysukumaran@gmail.com>
2026-01-21 13:58:51 -0800

    Set up factory, seeder

--- app/Models/Ninja.php
+++ app/Models/Ninja.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ninja extends Model
 {
+    protected $fillable = ['name', 'skill', 'bio'];
     /** @use HasFactory<\Database\Factories\NinjaFactory> */
     use HasFactory;
 }
--- database/migrations/2026_01_21_204816_create_ninjas_table.php
+++ database/migrations/2026_01_21_204816_create_ninjas_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('ninjas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->integer('skill');
+            $table->text('bio');
         });
     }
 
