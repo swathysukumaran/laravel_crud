4a992e96e1f289904300df1cc4a7e9448bffa2ac
swathysukumaran <swathysukumaran@gmail.com>
2026-01-20 16:48:03 -0800

    Add styles

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <header>
